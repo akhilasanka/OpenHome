859ad9af22ca49b6bad17eda35e4f85de661c8ff
Swetha Suresh <swetha.suresh@sjsu.edu>
2019-11-29 15:29:28 -0800

    Use property model

--- openhome-backend/src/main/java/com/cmpe275/openhome/service/PropertyService.java
+++ openhome-backend/src/main/java/com/cmpe275/openhome/service/PropertyService.java
@@ -2,10 +2,12 @@ package com.cmpe275.openhome.service;
 
 import java.util.List;
 
-import com.cmpe275.openhome.entity.Property;
+import com.cmpe275.openhome.model.Property;
 import com.cmpe275.openhome.entity.PropertyDetails;
 
 public interface PropertyService {
 	List<Property> getHardcodedPropertyList();
 	PropertyDetails getHardcodedPropertyDetails();
+
+	Property hostProperty(Property property);
 }
