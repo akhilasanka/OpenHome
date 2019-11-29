417e3991d890d69059b452f916a87fcc3356d0d2
Swetha Suresh <swetha.suresh@sjsu.edu>
2019-11-29 15:31:10 -0800

    Property post backend logic

--- openhome-backend/src/main/java/com/cmpe275/openhome/controller/PropertyController.java
+++ openhome-backend/src/main/java/com/cmpe275/openhome/controller/PropertyController.java
@@ -1,20 +1,24 @@
 package com.cmpe275.openhome.controller;
 
-import com.cmpe275.openhome.entity.Property;
 import com.cmpe275.openhome.entity.PropertyDetails;
+import com.cmpe275.openhome.model.Property;
+import com.cmpe275.openhome.payload.ApiResponse;
 import com.cmpe275.openhome.payload.PostPropertyRequest;
 import com.cmpe275.openhome.security.CurrentUser;
 import com.cmpe275.openhome.security.UserPrincipal;
 import com.cmpe275.openhome.service.PropertyService;
+import com.cmpe275.openhome.util.PropertyJsonToModelUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
-import java.util.Random;
 
 @RestController
 public class PropertyController {
@@ -30,7 +34,8 @@ public class PropertyController {
 
   @CrossOrigin(origins = "http://localhost:3000")
   @PostMapping("/hosts/{hostId}/property")
-  public int postProperty(@CurrentUser UserPrincipal userPrincipal, @PathVariable String hostId, @Valid @RequestBody PostPropertyRequest postPropertyRequest) {
+  @PreAuthorize("hasRole('USER')")
+  public ResponseEntity<?> postProperty(@CurrentUser UserPrincipal userPrincipal, @PathVariable String hostId, @Valid @RequestBody PostPropertyRequest postPropertyRequest) {
     System.out.println("HostId: " + hostId);
     ObjectMapper mapper = new ObjectMapper();
     try {
@@ -39,7 +44,22 @@ public class PropertyController {
     } catch (JsonProcessingException e) {
       e.printStackTrace();
     }
-    return new Random().nextInt(); // TODO: proper impl!
+
+    Property result = null;
+    try {
+      Property property = PropertyJsonToModelUtil.getProperty(postPropertyRequest);
+      result = myPropertyService.hostProperty(property);
+    } catch (Exception e) {
+      //send a failure status code like 500
+      e.printStackTrace();
+    }
+
+    URI location = ServletUriComponentsBuilder
+            .fromCurrentContextPath().path("/property")
+            .buildAndExpand(result.getId()).toUri();
+
+    return ResponseEntity.created(location)
+            .body(new ApiResponse(true, "Property registered successfully@"));
   }
 
   @CrossOrigin(origins = "http://localhost:3000")
