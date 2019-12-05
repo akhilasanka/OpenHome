40aaef102caa6f0b7c338de0061d7db31d30bf2c
Mario Yepez-Olvera <mario.yepez@sjsu.edu>
2019-12-05 09:06:35 -0800

    Fixed host cancelation logic
    guest should no longer be refunded full amt before check-in since they
    haven't paid

--- openhome-backend/src/main/java/com/cmpe275/openhome/service/ReservationService.java
+++ openhome-backend/src/main/java/com/cmpe275/openhome/service/ReservationService.java
@@ -303,20 +303,12 @@ public class ReservationService {
         	reservation.setStatus(ReservationStatusEnum.hostCanceledBeforeCheckIn);
         	updateReservation(reservation);
         	
-    		// refund guest entire amount
-        	Double totalRefund = payProcessingUtil.calculateTotalPrice(
-        			startDateTime.toLocalDate(),
-        			endDateTime.toLocalDate(), 
-    				reservation.getWeekdayPrice(), 
-    				reservation.getWeekendPrice(), 
-    				reservation.getDailyParkingPrice()
-    		);
-        	payProcessingUtil.recordPayment(reservation.getId(), ChargeType.GUESTREFUND, totalRefund);
-
-    		// if there are days within the 7 day range, host needs to pay 15% fee
-    		if (startDateTime.isBefore(currentDateTimePlus7Days)) {
+        	// check if there is an overlap between the dates
+        	boolean datesOverlap = startDateTime.isBefore(currentDateTimePlus7Days) && currentDateTime.isBefore(endDateTime);
+        	if (datesOverlap) {
+    			// if there are days within the 7 day range, host needs to pay 15% fee
     			
-    			// determine if Reservation End Date is before 6 Day Period
+    			// determine if Reservation End Date is before 7 Day Period
     			// pick the one that ends sooner
     			LocalDate penaltyEndDate;
     			if (currentDateTimePlus7Days.toLocalDate().isBefore(endDateTime.toLocalDate())) {
@@ -344,7 +336,7 @@ public class ReservationService {
     		reservation.setHostCancelationDate(cancelationDate);
         	reservation.setStatus(ReservationStatusEnum.pendingHostCancelation);
         	
-        	Date actualCheckOutDate = DateUtils.convertLocalDateTimeToDate(currentDateTime.plusDays(1).toLocalDate().atTime(15, 0));
+        	Date actualCheckOutDate = DateUtils.convertLocalDateTimeToDate(currentDateTime.plusDays(1).toLocalDate().atTime(11, 0));
         	reservation.setCheckOutDate(actualCheckOutDate);
         	updateReservation(reservation); 
         	
